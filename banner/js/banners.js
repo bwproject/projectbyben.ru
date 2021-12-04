c2dc6abcd0d41464f5153492b5c2739a90895383
Igor <linkthemrmes109@gmail.com>
2021-12-04 16:25:58 +0500

    Банеры
    Реклама баннеры OBS

---
+++ banner/js/banners.js
@@ -0,0 +1,82 @@
+const banners = [
+    {
+        name: "VK",
+        img: "./img/vk-brands.svg", 
+        background: "background: #00b3ff",
+        colorText: "#fff",
+        dynamicText: [
+		    `<strong>Подпишись на группу в ВК<br>будь в курсе всех новостей</strong>`,
+            `<strong>vk.com/<br>publicbyben</strong>`
+        ]
+    },
+    {
+        name: "empy",
+        colorText: "#fff",
+        dynamicText: [
+        ]
+    },	
+    {
+        name: "Twitch",
+        img: "./img/twitch-brands.svg",
+        background: "background: #b700ff",
+        colorText: "#fff",
+        dynamicText: [
+			`<strong>Стримы на твиче<br>подпишись что бы не пропустить</strong>`,
+            `<strong>twitch.tv/<br>streamsbyben</strong>`
+        ]
+    },
+    {
+        name: "empy2",
+        colorText: "#fff",
+        dynamicText: [
+        ]
+    },	
+    {
+        name: "YT",
+        img: "./img/youtube-brands.svg",
+        background: "background: #db120b",
+        colorText: "#fff",
+        dynamicText: [
+			`<strong>Канал на ютубе<br>Моменты со стримов и нарезки</strong>`,
+            `<strong>youtube.com/channel/<br>UCiJUr9eiijACrvV9F1kzhmA</strong>`
+        ]
+    },
+    {
+        name: "empy3",
+        colorText: "#fff",
+        dynamicText: [
+        ]
+    },
+    {
+        name: "Discord",
+        img: "./img/discord-brands.svg",
+        background: "background: #66ff00",
+        colorText: "#fff",
+        dynamicText: [
+			`<strong>Дискорд<br>Общаемся и тд</strong>`,
+            `<strong>discord.com/invite/<br>qQ2F9dP</strong>`
+        ]
+    },
+    {
+        name: "empy6",
+        colorText: "#fff",
+        dynamicText: [
+        ]
+    },
+	{
+        name: "SITE",
+        img: "./img/comment-dots-solid.svg",
+        background: "background: #66ff00",
+        colorText: "#fff",
+        dynamicText: [
+			`<strong>Мой сайт визитка<br>Много полезной информации</strong>`,
+            `<strong>Заходи смтори<br>streamsbyben.space</strong>`
+        ]
+    },
+    {
+        name: "empy8",
+        colorText: "#fff",
+        dynamicText: [
+        ]
+    }	
+];
